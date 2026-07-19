d9eea0c700f7d1b2102e9fa1c84b066f2b7feaa9
SpaceShaman <spaceshaman@tuta.io>
2026-07-19 14:07:46 +0200

    ✨ (main.zig): introduce constants for window dimensions and center ♻️ (main.zig): refactor window initialization to use constants 💡 (main.zig): change background color to black for better contrast ✨ (main.zig): add a white circle at the window center for visual interest

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,20 @@
 const rl = @import("raylib");
 
+const windowWidth = 1280;
+const windowHeight = 720;
+const windowCenterX = windowWidth / 2;
+const windowCenterY = windowHeight / 2;
+
 pub fn main() anyerror!void {
-    rl.initWindow(1280, 720, "Flipper");
+    rl.initWindow(windowWidth, windowHeight, "Flipper");
     defer rl.closeWindow();
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(.white);
+        rl.clearBackground(.black);
 
-        rl.drawText("Welcome in Flipper game ;)", 200, 310, 40, .black);
+        rl.drawCircle(windowCenterX, windowCenterY, 50, .white);
     }
 }
