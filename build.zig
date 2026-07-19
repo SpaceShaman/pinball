1160882c0a48bf65ab4e307fd5fdad3a47822ebb
SpaceShaman <spaceshaman@tuta.io>
2026-07-19 13:30:37 +0200

    🔧 (build.zig): add raylib and raygui dependencies and link them to the executable 🔧 (build.zig): allow passing arguments to the application in the build command ⬆️ (build.zig.zon): update fingerprint and minimum Zig version for compatibility

--- build.zig
+++ build.zig
@@ -41,6 +41,15 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
     // Here we define an executable. An executable needs to have a root module
     // which needs to expose a `main` function. While we could add a main function
     // to the module defined above, it's sometimes preferable to split business
@@ -83,6 +92,10 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    exe.root_module.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
+
     // This declares intent for the executable to be installed into the
     // install prefix when running `zig build` (i.e. when executing the default
     // step). By default the install prefix is `zig-out/` but can be overridden
@@ -111,7 +124,9 @@ pub fn build(b: *std.Build) void {
 
     // This allows the user to pass arguments to the application in the build
     // command itself, like this: `zig build run -- arg1 arg2 etc`
-    run_cmd.addPassthruArgs();
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
     // Creates an executable that will run `test` blocks from the provided module.
     // Here `mod` needs to define a target, which is why earlier we made sure to
