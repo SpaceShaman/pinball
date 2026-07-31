5e66f115d0db6ca0032c81ce69fc738696b90bda
SpaceShaman <spaceshaman@tuta.io>
2026-07-31 18:22:31 +0200

    ♻️ (collisions.zig): refactor collision depth reduction logic into separate function `reduceDepth` to improve code readability and maintainability

--- src/collisions.zig
+++ src/collisions.zig
@@ -14,24 +14,19 @@ pub fn resolveWallCollisions(ball: *Ball, walls: []const Wall) void {
 }
 
 fn resolveWallCollision(ball: *Ball, wall: Wall) void {
+    reduceDepth(ball, wall);
     const collision_normal = wall.collisionNormal(ball.position);
-
-    const distance = wall.distance(ball.position);
-    if (distance < ball.radius) {
-        const depth = ball.radius - distance;
-        ball.position = ball.position.add(collision_normal.negate().scale(depth));
-    }
-
     const velocity_n = collision_normal.scale(ball.velocity.dotProduct(collision_normal));
     const velocity_t = ball.velocity.subtract(velocity_n);
     const reduced_velocity_n = velocity_n.scale(ball.bounciness);
     ball.velocity = velocity_t.add(reduced_velocity_n.negate());
 }
 
-fn lineReflect(ball: *Ball, wall: Wall) Vector2 {
-    const start_end = wall.end.subtract(wall.start);
-    const start_point = ball.position.subtract(wall.start);
-    const t = start_end.dotProduct(start_point) / start_end.dotProduct(start_end);
-    const closest_point = wall.start.add(start_end.scale(t));
-    return closest_point.subtract(ball.position);
+fn reduceDepth(ball: *Ball, wall: Wall) void {
+    const distance = wall.distance(ball.position);
+    if (distance < ball.radius) {
+        const depth = ball.radius - distance;
+        const collision_normal = wall.collisionNormal(ball.position);
+        ball.position = ball.position.add(collision_normal.negate().scale(depth));
+    }
 }
