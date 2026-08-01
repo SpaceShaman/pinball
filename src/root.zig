539f14bd65d6be6bbf6852ec68918b0e7200d711
SpaceShaman <spaceshaman@tuta.io>
2026-08-01 15:18:37 +0200

    ♻️ (root.zig): reorder imports for better organization and readability

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
+pub const Game = @import("Game.zig");
+pub const Ball = @import("Ball.zig");
 pub const Wall = @import("Wall.zig");
 pub const Flipper = @import("Flipper.zig");
-pub const Ball = @import("Ball.zig");
-pub const Game = @import("Game.zig");
+pub const collisions = @import("collisions.zig");
 
 test {
     std.testing.refAllDecls(@This());
